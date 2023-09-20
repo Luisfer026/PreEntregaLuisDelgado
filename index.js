1a93452e154c8992c3468d2b5d4ab7700b9f34a1
Luis Delgado <luisfer02delgado@gmail.com>
2023-09-20 19:39:33 -0400

    preentrega3

--- index.js
+++ index.js
@@ -1,70 +1,99 @@
+const shopcontent = document.getElementById("shopcontent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
 const productos = [
-    {nombre:"hamburguesa",precio: 30},
-    {nombre:"papas fritas",precio: 10},
-    {nombre:"helado",precio: 15},
-    {nombre:"soda",precio: 10},
-    {nombre:"hot dog",precio: 15},  
+    {id:1,
+     nombre:"hamburguesa",
+     precio: 30,
+     img:"https://estaticos-cdn.elperiodico.com/clip/1a0a98f5-754d-49f3-a3dd-455d1b98203e_alta-libre-aspect-ratio_default_0.jpg",
+     },
+    {id:2,
+     nombre:"papas                     ",
+     precio: 10,
+     img:"https://images.unsplash.com/photo-1556710986-4a70434a76c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+     },
+    {id:3,
+     nombre:"helado",
+     precio: 15,
+     img:"https://images.unsplash.com/photo-1560008581-09826d1de69e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1944&q=80",
+    },
+     
+    {id:4,
+     nombre:"soda",
+     precio: 10,
+    img:"https://images.unsplash.com/photo-1624552184280-9e9631bbeee9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",},
+    {id:5,
+     nombre:"hot dog",
+     precio: 15,
+    img:"https://images.unsplash.com/photo-1541151040323-4d766525ec84?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",},  
 ];
 let carrito =[]
 
-let seleccion= prompt("hola desea comprar algun producto si o no")
+productos.forEach((product)=> {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p>${product.precio}</p>
 
+    `;
 
-while(seleccion !="si" && seleccion !="no"){
-    alert("por favor ingrese si o no")
-    seleccion = prompt("hola desea comprar algo si o no")
-}
+    shopcontent.append(content);
 
-if(seleccion =="si"){
-    alert("a continuacion nuestra lista de productos")
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
 
-    let todoslosProductos= productos.map((producto) => producto.nombre + " " + producto.precio + "$");
-    alert(todoslosProductos.join("-"))  
-} 
-else if(seleccion== "no"){
-alert("gracias por venir, hasta pronto.")
-}
-while(seleccion != "no"){
-    let producto= prompt("agrega un producto a tu carrito")
-    let precio = 0
-    if(producto == "hamburguesa" || producto =="papas fritas" || producto =="helado" || producto =="soda" || producto =="hot dog"){
-        switch(producto) {
-            case "hamburguesa":
-            precio = 30
-            break;
-            case "papas fritas":
-            precio = 10
-            break;
-            case "helado":
-            precio = 15
-            break;
-            case "soda":
-            precio = 10
-            break;
-            case "hot dog":
-            precio = 15
-            break;
-            default:
-            break;
-        }
-     let unidades = parseInt(prompt("cuantas unidades quiere llevar"))
+     content.append(comprar);
+
+     comprar.addEventListener("click", () =>{
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+
+        });
+        console.log(carrito);
+     });
+});
     
-     carrito.push({producto, unidades, precio})
-     console.log(carrito)
-    } else{
-        alert("no tenemos ese producto")
-     
-    }
+    verCarrito.addEventListener("click", ()=>{
+        modalContainer.innerHTML ="";
+        modalContainer.style.display = "flex"
+        const modalheader = document.createElement("div");
+        modalheader.className = "modal-header";
+        modalheader.innerHTML = `
+        <h2 class="modal-header-title">carrito.</h1>
+        `;
 
-    seleccion = prompt("desea seguir comprando?")
-    
-    while(seleccion === "no"){
-        alert("gracias por comprar, vuelva pronto")
-        carrito.forEach((carritofinal) =>{
-            console.log(`producto: ${carritofinal.producto}, unidades: ${carritofinal.unidades},total a pagar${carritofinal.unidades * carritofinal.precio}`)
-        })
-        break;
-    }
-}
-const total= carrito.reduce((acc, el)=> acc + el.precio * el.unidades, 0)
-console.log(`el total a pagar es: ${total}`);
+        modalContainer.append(modalheader);
+
+        const modalbutton = document.createElement("h3");
+        modalbutton.innerText = "x";
+        modalbutton.className = "modal-header-button";
+        modalbutton.addEventListener("click", ()=>{
+            modalContainer.style.display = "none";
+
+        });
+
+        modalheader.append(modalbutton)
+        carrito.forEach((product)=>{
+
+        let carritocontent = document.createElement("div")
+        carrito.className = "modal-content"
+        carrito.innerHTML = `
+        <img src="${product.img}">
+        <h4>${product.nombre}</h4>
+        <p>${productos.precio}$</p>
+        `;
+        modalContainer.append(carritocontent);
+    });
+    const total = carrito.reduce((acc, el)=> acc + el.precio, 0);
+
+    const totalBuying = document.createElement("div")
+    totalBuying.className = "total-content"
+    totalBuying.innerHTML = `total a pagar: ${total}`
+    modalContainer.append(totalBuying);
+    }); 
